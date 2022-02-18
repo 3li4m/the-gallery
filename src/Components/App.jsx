8733cc1a4e9d6f2a70f4dd5a182addcd7805b0c1
ElieAjaka <elie.ajaka87@gmail.com>
2022-02-18 19:34:34 +1100

    Test Update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,19 +2,23 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home"
 import About from "./About"
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
+
 function App(){
     return (
         <div>
-            <Router>
-                <Header/>
-                <Routes>
-                    <Route path="/the-gallery/" element={<Home />}></Route>
-                    <Route path="/the-gallery/about" element={<About />}></Route>
-                </Routes>
-            </Router>
-            
-            <Footer />
+            <HashRouter>
+            </HashRouter>
+
+                <Router>
+                    <Header/>
+                    <Routes>
+                        <Route path="/the-gallery/" element={<Home />}></Route>
+                        <Route path="/the-gallery/about" element={<About />}></Route>
+                    </Routes>
+                </Router>
+                
+                <Footer />
         </div>
     );
 }
