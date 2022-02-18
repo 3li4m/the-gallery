daba6c72b7d3d266ba026ca4253acd9794edff5d
ElieAjaka <elie.ajaka87@gmail.com>
2022-02-18 18:08:42 +1100

    Test Update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return(
     <div className="aboutContainer" >
         <div className="aboutImage">
-            <img src="./photo-gallery/images/photo.jpg" alt="developer"></img>
+            <img src="./images/photo.jpg" alt="developer"></img>
         </div>
 
         <div className="aboutInfo">
