54ec01d0fb4096d382221b31fc671bc077f31565
ElieAjaka <elie.ajaka87@gmail.com>
2022-02-19 14:53:08 +1100

    Test Update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,10 +18,6 @@ const About = () => {
             <p>I have a range of experience in diffrient technologies such as c#, python, and JavaScript and web technologies to create interactive and innovative
                 products to improve the experience and convenience of everyday users.
             </p>
-            <br />
-
-            <p >In my free time I like to to produce Electronic music, Go for hikes and bouldering.
-            </p>
             <div className="faHandler">
                 <a href="https://github.com/3li4m" ><i class="fa-class fa-brands fa-github"></i></a>
                 <a href = "mailto: contacteliea@gmail.com" ><i class="fa-class fa-solid fa-envelope-open"></i></a>
